d4b96c39d165e1bac74ef335617abebcc7b0cfef
Ruvindu234 <ruvindu234@gmail.com>
2026-03-26 16:06:00 +0530

    feat: Add Resources Page with FAQ, Video Section, and Header
    - Created ResourcesPage component to serve as the main entry for the resources section.
    - Implemented ResourcesPageHeader for the page header with navigation links.
    - Added Video component to showcase a video with a popup feature.
    - Developed Faq2 component featuring an accordion for frequently asked questions.
    - Introduced Accordion component to manage the accordion functionality.
    - Included a new FAQ banner image for visual enhancement.

---
+++ src/app/resources/page.js
@@ -0,0 +1,26 @@
+import Footer2 from "@/components/layout/footer/Footer2";
+import Header from "@/components/layout/header/Header";
+import ResourcesPageHeader from "@/components/sections/resources/ResourcesPageHeader";
+import Video from "@/components/sections/videos/Video";
+import Faq2 from "@/components/sections/faq/Faq2";
+import Cta1 from "@/components/sections/cta/Cta1";
+import TjMagicCursor from "@/components/shared/others/TjMagicCursor";
+import ClientWrapper from "@/components/shared/wrappers/ClientWrapper";
+
+export default function ResourcesPage() {
+	return (
+		<div>
+			<Header headerType={2} />
+			<Header isStickyHeader={true} headerType={2} />
+			<main>
+				<ResourcesPageHeader />
+				<Video />
+				<Faq2 />
+				<Cta1 />
+			</main>
+			<Footer2 />
+			<ClientWrapper />
+			<TjMagicCursor type={2} />
+		</div>
+	);
+}
