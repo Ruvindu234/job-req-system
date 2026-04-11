8d0480bac3fdcddac60271db7ffe769dad7b91f8
Ruvindu234 <ruvindu234@gmail.com>
2026-04-11 11:36:58 +0530

    Remove unused About2 and Testimonials2 components from Home2

--- src/app/home-02/page.js
+++ src/app/home-02/page.js
@@ -1,10 +1,8 @@
 import Footer2 from "@/components/layout/footer/Footer2";
 import Header from "@/components/layout/header/Header";
-import About2 from "@/components/sections/About/About2";
 import Contact1 from "@/components/sections/contacts/Contact1";
 import Cta1 from "@/components/sections/cta/Cta1";
 import Hero2 from "@/components/sections/heros/Hero2";
-import Testimonials2 from "@/components/sections/testimonials/Testimonials2";
 import TjMagicCursor from "@/components/shared/others/TjMagicCursor";
 import ClientWrapper from "@/components/shared/wrappers/ClientWrapper";
 
@@ -15,8 +13,6 @@ export default function Home2() {
 			<Header isStickyHeader={true} headerType={2} />
 			<main>
 				<Hero2 />
-				<About2 />
-				<Testimonials2 />
 				<Contact1 />
 				<Cta1 />
 			</main>
