fe7e3b103d2bcd468084cb8a2da184a21e05f099
Ruvindu234 <ruvindu234@gmail.com>
2026-04-11 12:28:11 +0530

    Remove unused EmployerEnquiry component from EmployersPage

--- src/app/employers/page.js
+++ src/app/employers/page.js
@@ -2,7 +2,6 @@ import Footer2 from "@/components/layout/footer/Footer2";
 import Header from "@/components/layout/header/Header";
 import EmployersPageHeader from "@/components/sections/employers/EmployersPageHeader";
 import EmployerContent from "@/components/sections/employers/EmployerContent";
-import EmployerEnquiry from "@/components/sections/employers/EmployerEnquiry";
 import Cta1 from "@/components/sections/cta/Cta1";
 import TjMagicCursor from "@/components/shared/others/TjMagicCursor";
 import ClientWrapper from "@/components/shared/wrappers/ClientWrapper";
@@ -15,8 +14,7 @@ export default function EmployersPage() {
 			<main>
 				<EmployersPageHeader />
 				<EmployerContent />
-				<EmployerEnquiry />
-				<Cta1 />
+<Cta1 />
 			</main>
 			<Footer2 />
 			<ClientWrapper />
