1445b3ed76a51def0fa82d52762e4dfb10c9bfdf
Ruvindu234 <ruvindu234@gmail.com>
2026-04-21 09:41:18 +0530

    Refactor Hero2 component: remove title and subtitle from slides, update background image styling

--- src/components/sections/heros/Hero2.js
+++ src/components/sections/heros/Hero2.js
@@ -48,23 +48,16 @@ const Hero2 = () => {
 				onSlideChange={swiper => updateDashWidth(swiper)}
 				className="full-slider-active"
 			>
-				{heroSlides.map(({ img, title, subtitle }, idx) => (
+				{heroSlides.map(({ img }, idx) => (
 					<SwiperSlide key={idx}>
 						<section
 							className="tj-slider-section"
-							style={{ backgroundImage: `url(${img})` }}
+							style={{ backgroundImage: `linear-gradient(rgba(5, 18, 31, 0.3), rgba(5, 18, 31, 0.1)), url(${img})`, minHeight: "100vh" }}
 						>
 							<div className="container">
 								<div className="row">
 									<div className="slider-wrapper">
 										<div className="slider-content">
-											<span className="sub-title">{subtitle}</span>
-											<h1 className="slider-title" style={{ fontSize: "54px" }}>{title}</h1>
-											<ButtonPrimary
-												text={"Free consultation"}
-												url={"/contact"}
-												className={"slider-button"}
-											/>
 										</div>
 									</div>
 								</div>
