471ed552bd57ffefb1d737e6e89c7655d2053dc8
Ruvindu234 <ruvindu234@gmail.com>
2026-05-20 11:22:05 +0530

    Update Hero2 component: modify transform style for enhanced image scaling

--- src/components/sections/heros/Hero2.js
+++ src/components/sections/heros/Hero2.js
@@ -66,7 +66,7 @@ const Hero2 = () => {
 									objectFit: "contain",
 									objectPosition: "center",
 									zIndex: 0,
-									transform: "scale(1.15)",
+									transform: "scaleX(1.45) scaleY(1.20)",
 								}}
 							/>
 							<div className="container" style={{ position: "relative", zIndex: 2 }}>
